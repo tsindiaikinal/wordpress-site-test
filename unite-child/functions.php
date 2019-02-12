182d4439da7e67a52862f45dc319828017cc7d00
Alex Geic <tsindalex@mail.ru>
2019-02-12 08:33:29 +0200

    add params for new type of data

--- unite-child/functions.php
+++ unite-child/functions.php
@@ -15,12 +15,25 @@ function theme_enqueue_styles() {
 // Моя функция: регистрирует новый тип данных и таксономии
 function films_library_init() {
 	    $args = array(
-	      'label' => 'Фильмы',
+		  'label' => 'null',
+		'labels' => array(
+			'name'               => 'Фильмы',
+			'singular_name'      => 'Фильм', 
+			'add_new'            => 'Добавить фильм', 
+			'add_new_item'       => 'Добавление фильма',
+			'edit_item'          => 'Редактирование фильма',
+			'new_item'           => 'Новый фильм',
+			'view_item'          => 'Посмотреть фильм',
+			'search_items'       => 'Найти фильм',
+			'not_found'          => 'Фильмов найдено', 
+			'not_found_in_trash' => 'В корзине фильмов не найдено',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Фильмы', // название меню
+		),
 	        'public' => true,
 			'show_ui' => true,
 	        'capability_type' => 'post',
 	        'hierarchical' => false,
-	        'rewrite' => array('slug' => 'films'),
 	        'query_var' => true,
 			'menu_position' => 9,
 	        'menu_icon' => 'dashicons-video-alt',
@@ -34,16 +47,20 @@ function films_library_init() {
 	            'revisions',
 	            'thumbnail',
 	            'author',
-	            'page-attributes',)
+				'page-attributes',),
+			'show_in_rest'    => true,
+			'rest_base'       => films,
+			'rest_controller_class' => 'WP_REST_Posts_Controller',
+			'taxonomies'          => array(),
+			'has_archive'         => false,
+			'rewrite'             => false,
 	        );
 	    register_post_type( 'cinema', $args );
 	}
 	add_action( 'init', 'films_library_init' );
 
-
-add_action('init', 'create_taxonomy');
-function create_taxonomy(){
-	// список параметров можно посмотреть на: http://wp-kama.ru/function/get_taxonomy_labels
+add_action( 'init', 'create_cinema_taxonomies');
+function create_cinema_taxonomies(){
 	register_taxonomy('gengres', array('cinema'), array(
 		'label'                 => '', // определяется параметром $labels->name
 		'labels'                => array(
@@ -60,30 +77,26 @@ function create_taxonomy(){
 			'new_item_name'     => 'Новое имя жанра',
 			'menu_name'         => 'Жанры',
 		),
-		'description'           => '', // описание таксономии
+		'description'           => '',
 		'public'                => true,
-		'publicly_queryable'    => null, // равен аргументу public
-		'show_in_nav_menus'     => true, // равен аргументу public
-		'show_ui'               => true, // равен аргументу public
-		'show_in_menu'          => true, // равен аргументу show_ui
-		'show_tagcloud'         => true, // равен аргументу show_ui
-		'show_in_rest'          => null, // добавить в REST API
-		'rest_base'             => null, // $taxonomy
+		'publicly_queryable'    => true, 
+		'show_in_nav_menus'     => true, 
+		'show_ui'               => true, 
+		'show_in_menu'          => true, 
+		'show_tagcloud'         => true, 
+		'show_in_rest'          => true, // добавить в REST API
+		'rest_base'             => null, 
 		'hierarchical'          => true,
 		'update_count_callback' => '',
 		'rewrite'               => true,
 		//'query_var'             => $taxonomy, // название параметра запроса
 		'capabilities'          => array(),
-		'meta_box_cb'           => post_categories_meta_box, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
-		'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
+		'meta_box_cb'           => post_categories_meta_box, // callback post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
+		'show_admin_column'     => false,
 		'_builtin'              => false,
-		'show_in_quick_edit'    => null, // по умолчанию значение show_ui
+		'show_in_quick_edit'    => null,
 	) );
-}
 
-
-add_action( 'init', 'create_cinema_taxonomies');
-function create_cinema_taxonomies(){
 	register_taxonomy('countries', array('cinema'), array(
 		'label'                 => '', // определяется параметром $labels->name
 		'labels'                => array(
@@ -100,15 +113,15 @@ function create_cinema_taxonomies(){
 			'new_item_name'     => '',
 			'menu_name'         => 'Страны',
 		),
-		'description'           => '', // описание таксономии
+		'description'           => '',
 		'public'                => true,
-		'publicly_queryable'    => null, // равен аргументу public
-		'show_in_nav_menus'     => true, // равен аргументу public
-		'show_ui'               => true, // равен аргументу public
-		'show_in_menu'          => true, // равен аргументу show_ui
-		'show_tagcloud'         => true, // равен аргументу show_ui
+		'publicly_queryable'    => true, 
+		'show_in_nav_menus'     => true, 
+		'show_ui'               => true, 
+		'show_in_menu'          => true, 
+		'show_tagcloud'         => true, 
 		'show_in_rest'          => null, // добавить в REST API
-		'rest_base'             => null, // $taxonomy
+		'rest_base'             => null, 
 		'hierarchical'          => true,
 		'update_count_callback' => '',
 		'rewrite'               => true,
@@ -119,6 +132,7 @@ function create_cinema_taxonomies(){
 		'_builtin'              => false,
 		'show_in_quick_edit'    => null, 
 		) );
+
 		register_taxonomy('year', array('cinema'), array(
 		'label'                 => '', // определяется параметром $labels->name
 		'labels'                => array(
@@ -135,15 +149,15 @@ function create_cinema_taxonomies(){
 			'new_item_name'     => '',
 			'menu_name'         => 'Годы',
 		),
-		'description'           => '', // описание таксономии
+		'description'           => '',
 		'public'                => true,
-		'publicly_queryable'    => null, // равен аргументу public
-		'show_in_nav_menus'     => true, // равен аргументу public
-		'show_ui'               => true, // равен аргументу public
-		'show_in_menu'          => true, // равен аргументу show_ui
-		'show_tagcloud'         => true, // равен аргументу show_ui
+		'publicly_queryable'    => true, 
+		'show_in_nav_menus'     => true, 
+		'show_ui'               => true, 
+		'show_in_menu'          => true, 
+		'show_tagcloud'         => true, 
 		'show_in_rest'          => null, // добавить в REST API
-		'rest_base'             => null, // $taxonomy
+		'rest_base'             => null, 
 		'hierarchical'          => true,
 		'update_count_callback' => '',
 		'rewrite'               => true,
@@ -170,15 +184,15 @@ function create_cinema_taxonomies(){
 			'new_item_name'     => '',
 			'menu_name'         => 'Актеры',
 		),
-		'description'           => '', // описание таксономии
+		'description'           => '',
 		'public'                => true,
-		'publicly_queryable'    => null, // равен аргументу public
-		'show_in_nav_menus'     => true, // равен аргументу public
-		'show_ui'               => true, // равен аргументу public
-		'show_in_menu'          => true, // равен аргументу show_ui
-		'show_tagcloud'         => true, // равен аргументу show_ui
-		'show_in_rest'          => null, // добавить в REST API
-		'rest_base'             => null, // $taxonomy
+		'publicly_queryable'    => true, 
+		'show_in_nav_menus'     => true, 
+		'show_ui'               => true, 
+		'show_in_menu'          => true, 
+		'show_tagcloud'         => true, 
+		'show_in_rest'          => null, 
+		'rest_base'             => null,
 		'hierarchical'          => true,
 		'update_count_callback' => '',
 		'rewrite'               => true,
@@ -190,3 +204,15 @@ function create_cinema_taxonomies(){
 		'show_in_quick_edit'    => null, 
 		) );
 }
+
+
+function add_taxonomy_films(){
+	register_taxonomy_for_object_type( 'gengres', 'cinema');
+}
+add_action( 'init', 'add_taxonomy_films' );
+
+
+// $taxonomies = get_taxonomies();
+// foreach( $taxonomies as $taxonomy ) {
+// 	echo '<p>'. $taxonomy. '</p>';
+// }
